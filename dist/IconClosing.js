7b6e1d7812edd818e1052eeab8829470b5597bbc
Djoudi21 <abdelkrim@gdpr.dev>
2023-06-09 11:07:49 +0200

    first commit

--- dist/IconClosing.js
+++ dist/IconClosing.js
@@ -6,12 +6,20 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.IconClosing = IconClosing;
 var _react = _interopRequireDefault(require("react"));
-function IconClosing() {
-  return /*#__PURE__*/_react.default.createElement("svg", {
+require("./Modal.css");
+function IconClosing(_ref) {
+  var onClick = _ref.onClick;
+  function handleClick() {
+    onClick();
+  }
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "modal-icon",
+    onClick: handleClick
+  }, /*#__PURE__*/_react.default.createElement("svg", {
     fill: "none",
-    height: "20",
+    height: "40",
     viewBox: "0 0 20 20",
-    width: "20",
+    width: "40",
     xmlns: "http://www.w3.org/2000/svg"
   }, /*#__PURE__*/_react.default.createElement("rect", {
     fill: "none",
@@ -23,5 +31,5 @@ function IconClosing() {
     d: "M9.92484 10.685L12.8071 13.5673C12.9323 13.6925 13.1353 13.6925 13.2604 13.5673L13.5448 13.2829C13.67 13.1577 13.67 12.9548 13.5448 12.8296L10.6626 9.9473L13.5448 7.06504C13.67 6.93986 13.67 6.73689 13.5448 6.6117L13.2604 6.32729C13.1353 6.2021 12.9323 6.2021 12.8071 6.32729L9.92484 9.20955L7.04258 6.32729C6.9174 6.2021 6.71443 6.2021 6.58924 6.32729L6.30483 6.6117C6.17964 6.73689 6.17964 6.93986 6.30483 7.06504L9.18708 9.9473L6.30483 12.8296C6.17964 12.9548 6.17964 13.1577 6.30483 13.2829L6.58924 13.5673C6.71443 13.6925 6.9174 13.6925 7.04258 13.5673L9.92484 10.685Z",
     fill: "#3A3837",
     fillRule: "evenodd"
-  }));
+  })));
 }
